3b84a324f081656720f14043905f8dad6ba1349b
user <mihao85@gmail.com>
2025-06-23 15:01:16 +0700

    4.3.6. Math.abs [#373616 #546696]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -1,5 +1,4 @@
 package ru.job4j.condition;
-
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -1,5 +1,4 @@
 package ru.job4j.condition;
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
