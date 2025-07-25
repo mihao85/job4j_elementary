1b6a4d00f66c9ff9d3c05eafd2cf8be74018a8d9
user <mihao85@gmail.com>
2025-07-25 19:26:04 +0700

    6.4.0. Слово начинается с ... [#41585 #549069]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,5 +1,4 @@
 package ru.job4j.array;
-
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
--- src/test/java/ru/job4j/array/ArrayCharTest.java
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -1,7 +1,5 @@
 package ru.job4j.array;
-
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ArrayCharTest {
