0bc250c25a6d9f0ae53bac34ae44d820b8c3072c
user <mihao85@gmail.com>
2025-10-10 00:56:09 +0700

    6.7.4. Выигрышные комбинации в сокобан [#53859]

---
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class TwoNumberSum {
+    public static int[] getIndexes(int[] array, int target) {
+        int i = 0;
+        int j = 0;
+        while (i <= j) {
+            if (array[i]+array[j] == target) {
+              return new int[]{i, j};
+            }
+            if (array[i]+array[j] != target) {
+                i++;
+            }
+            j++;
+
+        }
+        return new int[0];
+    }
+}
