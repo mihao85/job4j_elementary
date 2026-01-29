6abf7b5f6216bd1104d5bf379303ac99a4cae951
user <mihao85@gmail.com>
2026-01-29 15:25:26 +0300

    4. Переопределение метода [#174890]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,5 +1,4 @@
 package ru.job4j.inheritance;
-
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
         return "{"
--- src/test/java/ru/job4j/inheritance/JSONReportTest.java
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -3,7 +3,6 @@ package ru.job4j.inheritance;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class JSONReportTest {
 
